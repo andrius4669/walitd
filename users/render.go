ccd11d30d2f060bdce335bd27d0c88b86615718d
algvai <algirdas.vainauskas@ktu.edu>
2016-05-21 18:16:32 +0300

    changes

--- users/render.go
+++ users/render.go
@@ -1,1 +1,33 @@
 package users
+
+
+func renderLoginPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Login page not implemented", 501);
+}
+func renderCreateFriendListPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Register page not implemented", 501);
+}
+func renderCreateGroupPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Create group page not implemented", 501);
+}
+func renderFriendListPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Friend list page not implemented", 501);
+}
+func renderGroupPage(w http.ResponseWriter, r *http.Request, id uint32)  {
+	http.Error(w, "Group  page not implemented", 501);
+}
+func renderGroupsPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Groups page not implemented", 501);
+}
+func renderMessagesPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Messages page not implemented", 501);
+}
+func renderProfilePage(w http.ResponseWriter, r *http.Request, id uint32)  {
+	http.Error(w, "Profile page not implemented", 501);
+}
+func renderRegisterPage(w http.ResponseWriter, r *http.Request)  {
+	http.Error(w, "Register page not implemented", 501);
+}
+
+
+
