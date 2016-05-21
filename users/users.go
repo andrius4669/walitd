96204ba098531dd3b24ce987772196eb2798d845
algvai <algirdas.vainauskas@ktu.edu>
2016-05-21 18:57:18 +0300

    changes

--- users/users.go
+++ users/users.go
@@ -32,5 +32,46 @@ func LoadTemplates() {
 // * some number
 
 func HandleRequest(w http.ResponseWriter, r *http.Request, pathi int) {
-	http.Error(w, fmt.Sprintf("501 not handlied: %s", r.URL.Path[pathi+1:]), 501)
+	rpath := r.URL.Path[pathi+1:]
+	if r.Method == "GET" || r.Method == "HEAD" {
+		if rpath == "" {
+			renderArticlesList(w, r)
+			return
+		}
+		i := str.IndexByte(rpath, '/')
+		if i == -1 {
+			// syntax is /zzz/ not /zzz in all GET/HEAD cases
+			http.Redirect(w, r, r.URL.Path+"/", http.StatusFound)
+			return
+		}
+		if rpath[:i] == "articles" {
+			if rpath[i+1:] == "" {
+				// Display list of news
+				renderArticlesList(w, r)
+			} else {
+			}
+			return
+		}
+
+		if rpath == "" {
+
+			return
+		}
+		i = str.IndexByte(rpath, '/')
+
+
+		if rpath == "" {
+			// be lazy there :^)
+			http.Redirect(w, r, "../", http.StatusFound)
+			return
+		}
+
+		http.NotFound(w, r)
+		return
+	} else if r.Method == "POST" {
+		// TODO(andrius) decide & implement posting
+		http.Error(w, "501 POST routines not yet implemented", 501)
+	} else {
+		http.Error(w, "501 method not implemented", 501)
+	}
 }
