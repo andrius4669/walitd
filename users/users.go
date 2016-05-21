a3804ac729ee7c5b1295684f78189c59f29f2d30
algvai <algirdas.vainauskas@ktu.edu>
2016-05-21 17:46:24 +0300

    changes

--- users/users.go
+++ users/users.go
@@ -18,47 +18,15 @@ func LoadTemplates() {
 	render.Load("profile", "users/profile.tmpl")
 	render.Load("register", "users/register.tmpl")
 }
+// users/createfriendlist
+// users/creategroup
+// users/friendlist
+// users/group/*
+// users/groups
+// users/login
+// users/messages
+// users/profile/*
+// users/register
 
-//func HandleRequest(w http.ResponseWriter, r *http.Request, pathi int) {
-//	rpath := r.URL.Path[pathi+1:]
-//	if r.Method == "GET" || r.Method == "HEAD" {
-//		if rpath == "" {
-//			// display list of boards
-////			renderBoardList(w, r)
-//			//TODO: show main page
-//			return
-//		}
-//		i := str.IndexByte(rpath, '/')
-//		if i == -1 {
-//			// syntax is /zzz/ not /zzz in all GET/HEAD cases
-//			http.Redirect(w, r, r.URL.Path+"/", http.StatusFound)
-//			return
-//		}
-//
-//	} else if r.Method == "POST" {
-//		// TODO(andrius) decide & implement posting
-//		http.Error(w, "501 POST routines not yet implemented", 501)
-//	} else {
-//		http.Error(w, "501 method not implemented", 501)
-//	}
-//}
-//func sayhelloName(w http.ResponseWriter, r *http.Request) {
-//	r.ParseForm()  // parse arguments, you have to call this by yourself
-//	fmt.Println(r.Form)  // print form information in server side
-//	fmt.Println("path", r.URL.Path)
-//	fmt.Println("scheme", r.URL.Scheme)
-//	fmt.Println(r.Form["url_long"])
-//	for k, v := range r.Form {
-//		fmt.Println("key:", k)
-//		fmt.Println("val:", strings.Join(v, ""))
-//	}
-//	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
-//}
-//
-//func main() {
-//	http.HandleFunc("/", sayhelloName) // set router
-//	err := http.ListenAndServe(":9090", nil) // set listen port
-//	if err != nil {
-//		log.Fatal("ListenAndServe: ", err)
-//	}
-//}
+//main page: users/groups if not loged in users/login
+// * some number
